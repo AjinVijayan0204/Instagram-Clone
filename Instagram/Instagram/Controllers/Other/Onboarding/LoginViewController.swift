d5ed58e18fc2a280951e3fe8ac6617f8847a03fa
AjinVijayan0204 <ajinvijayan0204@gmail.com>
2023-08-15 15:50:19 +0530

    created login screen components

--- Instagram/Instagram/Controllers/Other/Onboarding/LoginViewController.swift
+++ Instagram/Instagram/Controllers/Other/Onboarding/LoginViewController.swift
@@ -9,14 +9,47 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    private let usernameEmailField: UITextField = {
+        return UITextField()
+    }()
+    
+    private let passwordField: UITextField = {
+        let field = UITextField()
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let loginButton: UIButton = {
+        return UIButton()
+    }()
+    private let termsButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let privacyButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let headerView: UIView = {
+        return UIView()
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
     }
     
-
+    private func addSubViews(){
+        view.addSubview(usernameEmailField)
+        view.addSubview(passwordField)
+        view.addSubview(loginButton)
+        view.addSubview(termsButton)
+        view.addSubview(privacyButton)
+        view.addSubview(headerView)
+        
+    }
     
 
 }
