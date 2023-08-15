65c12660cf1147016ffce9424994d132a653a035
AjinVijayan0204 <ajinvijayan0204@gmail.com>
2023-08-15 15:04:27 +0530

    Common management files added in resources

---
+++ Instagram/Instagram/Resources/StorageManager.swift
@@ -0,0 +1,14 @@
+//
+//  StorageManager.swift
+//  Instagram
+//
+//  Created by Ajin on 15/08/23.
+//
+
+import Foundation
+
+public class StorageManager{
+    
+    static let shared = StorageManager()
+    
+}
